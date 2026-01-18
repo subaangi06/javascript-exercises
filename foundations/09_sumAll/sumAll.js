96a2c0d66699e861d77250c4c10955041ee52baf
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-18 14:09:56 +0530

    Modify function such that first test is passed

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let biggerNum =0;
+    let smallerNum = 0;
+    if (num1>num2){
+        biggerNum = num1;
+        smallerNum = num2;
+    } else if (num2>num1){
+        biggerNum = num2;
+        smallerNum = num1;
+    } else{
+        return num1;
+    }
+    let sum = 0;
+    for (let i = smallerNum; i<=biggerNum; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
