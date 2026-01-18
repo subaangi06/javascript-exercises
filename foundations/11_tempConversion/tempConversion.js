0c6f69039262b0e5dceebaf72ab206a5ac50bf97
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-18 15:43:05 +0530

    Write function such that it passes first test

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  temperature = ((temperature-32)*(5/9));
+  if (temperature != temperature.toFixed(1)){
+    temperature = temperature.toFixed(1);
+  }
+  return temperature;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  temperature = (temperature*(9/5)+32).toFixed(1);
+  return temperature;
 };
 
 // Do not edit below this line
