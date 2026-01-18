9eae9bf8c61b564c77f669333f1a066cb5b0a097
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-18 11:55:48 +0530

    Make changes to allow working of repeatString function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let outputString = "";
+    if (num<0) {
+        return "ERROR";
+    } else {
+        for (let i=0; i<=num; i++){
+            if (i==0){
+                continue;
+            }
+            outputString=outputString+string;
+        }
+        return outputString;
+    }
 };
 
 // Do not edit below this line
