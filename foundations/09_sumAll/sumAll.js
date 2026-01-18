98bb704869f5c4eaf6930b178d056ca217967f63
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-18 14:44:51 +0530

    Ensure function passes all tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,9 @@
 const sumAll = function(num1, num2) {
     let biggerNum =0;
     let smallerNum = 0;
+    if (num1<0 || num2<0||Number.isInteger(num1)!=true||Number.isInteger(num2)!=true){
+        return "ERROR";
+    }
     if (num1>num2){
         biggerNum = num1;
         smallerNum = num2;
