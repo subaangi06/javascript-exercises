89adc9203d2c6f4d083ab368fc8356c762046204
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-18 13:41:51 +0530

    Complete function + passes initial test

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, count) {
+    let numUserArg = arguments.length-1
+    for (let i = 1; i<=numUserArg; i++){
+        let currentArg = (element) => element == arguments[i];
+        let index = array.findIndex(currentArg);
+        array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+console.log(removeFromArray([1, 2, 3, 4], 4,2));
