f22434516bb615f90db4be03bccccc4259773dbc
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-18 15:21:24 +0530

    Complete function + ensure it passes all tests

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisibleByFour = year%4 ===0;
+    const century = year%100===0;
+    const isDivisibleByFourHundred = year%400===0;
 
+    if (isDivisibleByFour&&!century||isDivisibleByFourHundred){
+        return true;
+    } else {return false;}
 };
 
 // Do not edit below this line
