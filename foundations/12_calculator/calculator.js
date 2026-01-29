98e7e67140f765d3823f815b198636d0737177d0
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-29 15:49:35 +0530

    Code all functions, ensuring all tests are passed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	 return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, number) => total=total+number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, number)=> product*number, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorial = 1;
+  if (number==0){
+    return 1;
+  }
+  for (let i = number; i>0; i--){
+    factorial=factorial*i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
