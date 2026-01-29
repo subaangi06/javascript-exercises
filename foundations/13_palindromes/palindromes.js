d2f3c0b8adb5fca3668bbf9a92ec3ffc1899c175
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-29 16:02:21 +0530

    Add function such that first test is passed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const reversedWord = text.split("").reverse().join("").toLowerCase(); //splits each character into an array, then reverses the array, then joins the array back into a word
+    return text==reversedWord;
 };
 
 // Do not edit below this line
