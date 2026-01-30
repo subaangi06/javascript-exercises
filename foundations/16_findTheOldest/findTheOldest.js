9d89627629d18b5bfc6fe0446b07b5eee2838fb5
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-30 12:12:59 +0530

    Complete function such that it passes all tests

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const getAge = function(birth, death){
+    if (!death){
+        death = new Date().getFullYear();
+    }
+    return death-birth;
+}                                       
 
+const findTheOldest = function(peopleArray) {
+    return peopleArray.reduce((oldest,currentPerson)=>{
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return currentPersonAge>oldestAge ?currentPerson : oldest; //updating accumulator
+    });
+    
 };
 
+//result of applying reudce on an array returns the accumulator's value.
 // Do not edit below this line
 module.exports = findTheOldest;
