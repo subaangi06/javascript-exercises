b15e19bc1059b673c78c1d442990d7d3726e87da
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-30 11:15:35 +0530

    Write function such that it passes first test

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    position = parseInt(position);
+    if (position===1 || position === 2){
+        return 1;
+    }
+    let first = 1
+    let second = 1
+    let currentValue = 0;
+    for (let i = 2; i<position;i++){
+        currentValue = first+second;
+        first = second;
+        second = currentValue;
+    }
+    return currentValue;
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
